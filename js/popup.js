863fe8e7f9e37e6d2237ea83016576e075892fc0
wtnb93 <wtnb93@users.noreply.github.com>
2018-06-01 21:59:31 +0900

    no message

--- js/popup.js
+++ js/popup.js
@@ -14,22 +14,66 @@ const config = {
 }
 
 // popup.htmlで選択された時に実行
-chrome.storage.local.set(config, () => {
-  console.log('Settings saved')
-})
+chrome.storage.local.set(config, () => {})
 
 chrome.storage.local.get(config, value => {
-  $('#circle-size').text(value.size)
-  console.log(value.size)
-  for (const x of [...Array(value.size).keys()]) {
-    $('.container').append('<div class="row"><div class="col" style="background: blue; color: #fff; text-align: center;">size</div><div class="col"><div class="dropdown"><button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">4</button><div class="dropdown-menu" aria-labelledby="dropdownMenu2"></div></div></div></div>')
+  $('#size').val(value.size)
+  draw(value.size)
+})
+
+$(document).on('change', 'select#size', function() {
+  config.size = $(this).val()
+  chrome.storage.local.set(config, () => {})
+
+  draw(config.size)
+})
+
+const draw = (count) => {
+  $('ul').empty()
+  for (const x of [...Array(Number(count)).keys()]) {
+    $('ul').append(`
+      <ol>
+        <div style="display: flex; align-items: center;">
+          <div>
+            <canvas id="canvas-${x}" width="120" height="120"></canvas>
+          </div>
+        <div>
+        <select id="command">
+          <option value="back">back</option>
+          <option value="forward">forward</option>
+          <option value="4">createTab</option>
+          <option value="5">closeTab</option>
+          <option value="6">restoreTab</option>
+          <option value="7">hoge</option>
+          <option value="8">none</option>
+        </select>
+        </div>
+        </div>
+      </ol>`
+    )
+
+    const canvas = document.getElementById(`canvas-${x}`)
+    const context = canvas.getContext('2d')
+    // context.clearRect(0, 0, canvas.width, canvas.height)
+    context.lineWidth = 2
+    const step = Math.PI / config.size
+    const degree = step * 2
+
+    let begin = Math.PI * 2 - step
+    let end = step
+
+    for (const i of [...Array(Number(count)).keys()]) {
+      context.fillStyle = x === i ? config.activeRBGA : config.inactiveRBGA
+      // context.moveTo(50, 50)
+      context.beginPath()
+      context.arc(60, 60, 50, begin, end, false)
+      context.lineTo(60, 60)
+      context.closePath()
+      context.fill()
+      context.stroke()
+
+      begin = end
+      end = begin + degree
+    }
   }
-})
-
-console.log(document.getElementById('btn'))
-$('#btn').text('hogehoge')
-
-$(document).on('click', '#btn', function() {
-  console.log('clicked')
-  $(this).text('clicked')
-})
+}
